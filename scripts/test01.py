dd111ecdf03e30dfc58de1044d1f429e4233f0aa
LGY2008 <150691555@qq.com>
2019-03-19 17:30:15 +0800

    01

---
+++ scripts/test01.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+import allure
+import pytest
+
+sys.path.append(os.getcwd())
+
+"""
+    注意：
+        1. @allure.step()只能修饰函数名
+        2. allure.attach()只能修饰函数内主体内容
+"""
+
+class Test01:
+    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
+    @allure.step("新增地址方法被执行")
+    def test001(self):
+        allure.attach("点击新增地址按钮", "")
+        allure.attach("点击新增输入收件人：","")
+        allure.attach("点击新增输入手机号：","")
+        allure.attach("点击新增输入收货地址：","")
+        allure.attach("点击新增输入点击新增：","")
+        print("test001被执行")
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    # allure.attach("点击新增输入点击更新：", "")
+    @allure.step("更新地址方法被执行")
+    def test002(self):
+        print("test002被执行")
+
+
+    # 失败截图并写入报告
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    def test003(self):
+        print("断言失败，截图并写入报告")
+        with open("./image/faile.png","rb")as f:
+            allure.attach("失败原因：", f.read(), allure.attach_type.PNG)
+            # assert False
+
+    # 扩展
+    @allure.severity("trivial")
+    def test004(self):
+        print("test004被执行")
